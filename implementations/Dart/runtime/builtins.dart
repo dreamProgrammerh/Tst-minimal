fc53f11a62c43c20c05fdbd7363eb590d8d8f3a4
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2026-01-18 01:38:04 +0330

    update File(`builtins.dart`)
     - rename second rgba function to rgb.
     - start using runtime errors instead of exceptions.

--- implementations/Dart/runtime/builtins.dart
+++ implementations/Dart/runtime/builtins.dart
@@ -3,8 +3,10 @@ import 'values.dart';
 
 void initBuiltin() {
   registerFunction('rgba', (args) {
-    if (args.length != 4)
-      throw FormatException('rgba expects 4 arguments');
+    if (args.length != 4) {
+      RuntimeState.error('rgba expects 4 arguments');
+      return InvalidValue.instance;
+    }
 
     int r = args[0].asInt() & 0xff;
     int g = args[1].asInt() & 0xff;
@@ -14,15 +16,16 @@ void initBuiltin() {
     return IntValue((r << 24) | (g << 16) | (b << 8) | a);
   });
 
-  registerFunction('rgba', (args) {
-    if (args.length != 4)
-      throw FormatException('rgba expects 4 arguments');
+  registerFunction('rgb', (args) {
+    if (args.length != 3) {
+      RuntimeState.error('rgb expects 3 arguments');
+      return InvalidValue.instance;
+    }
 
     int r = args[0].asInt() & 0xff;
     int g = args[1].asInt() & 0xff;
     int b = args[2].asInt() & 0xff;
-    int a = args[3].asInt() & 0xff;
 
-    return IntValue((r << 24) | (g << 16) | (b << 8) | a);
+    return IntValue((r << 24) | (g << 16) | (b << 8) | 0xff);
   });
 }
