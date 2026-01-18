1d9dab8e8246f9330d6db7c3ac60082e4882da57
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2026-01-18 21:36:31 +0330

    update File(`builtins.dart`): register new colors functions (hex, hslo, hsl, hsvo, hsv).

--- implementations/Dart/runtime/builtins.dart
+++ implementations/Dart/runtime/builtins.dart
@@ -1,3 +1,4 @@
+import '../utils/colors.dart' as Colors;
 import 'context.dart';
 import 'values.dart';
 
@@ -41,5 +42,70 @@ void initBuiltin() {
     int b = args[2].asInt() & 0xff;
 
     return IntValue((0xff << 24) | (r << 16) | (g << 8) | b);
+  });
+
+  registerFunction('hex', (args) {
+    if (args.length != 1) {
+      RuntimeState.error('hex expects 1 arguments');
+      return InvalidValue.instance;
+    }
+
+    int code = args[0].asInt();
+
+    return IntValue(Colors.hex(code));
+  });
+  
+  registerFunction('hslo', (args) {
+    if (args.length != 4) {
+      RuntimeState.error('hslo expects 4 arguments');
+      return InvalidValue.instance;
+    }
+
+    double h = args[0].asFloat();
+    double s = args[1].asFloat();
+    double l = args[2].asFloat();
+    double o = args[2].asFloat();
+
+    return IntValue(Colors.hsl(h, s, l, o));
+  });
+  
+  registerFunction('hsl', (args) {
+    if (args.length != 3) {
+      RuntimeState.error('hsl expects 3 arguments');
+      return InvalidValue.instance;
+    }
+
+    double h = args[0].asFloat();
+    double s = args[1].asFloat();
+    double l = args[2].asFloat();
+
+    return IntValue(Colors.hsl(h, s, l));
+  });
+  
+  registerFunction('hsvo', (args) {
+    if (args.length != 4) {
+      RuntimeState.error('hsvo expects 4 arguments');
+      return InvalidValue.instance;
+    }
+
+    double h = args[0].asFloat();
+    double s = args[1].asFloat();
+    double v = args[2].asFloat();
+    double o = args[2].asFloat();
+
+    return IntValue(Colors.hsv(h, s, v, o));
+  });
+  
+  registerFunction('hsv', (args) {
+    if (args.length != 3) {
+      RuntimeState.error('hsv expects 3 arguments');
+      return InvalidValue.instance;
+    }
+
+    double h = args[0].asFloat();
+    double s = args[1].asFloat();
+    double v = args[2].asFloat();
+
+    return IntValue(Colors.hsv(h, s, v));
   });
 }
