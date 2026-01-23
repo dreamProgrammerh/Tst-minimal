ce08f72b204e3bda0201b0f7f8a122ac13af169e
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2026-01-23 18:54:21 +0330

    update File(`interpreter.dart`): new interpreter arguments '.time'
    Show time taken by the runner from diffrent angles (lexer, parser, eval,
    compile, all)

--- implementations/Dart/utils/interpreter.dart
+++ implementations/Dart/utils/interpreter.dart
@@ -35,6 +35,35 @@ enum OutputMode {
   }
 }
 
+enum TimeMode {
+  none,
+  lexer,
+  parser,
+  eval,
+  compile,
+  all;
+
+  static String? format(int qs) {
+    if (qs < 1000)
+      return "${qs}qs";
+      
+    else if (qs < 1_000_000)
+      return "${(qs * 0.001).toStringAsFixed(2)}ms";
+      
+    else if (qs < 1_000_000_000)
+      return "${(qs * 0.000001).toStringAsFixed(2)}s";
+      
+    else if (qs < 60_000_000_000)
+      return "${(qs * 0.000000001).toStringAsFixed(2)}s";
+      
+    else {
+      final int m = qs ~/ 60_000_000_000;
+      final int s = ((qs % 60_000_000_000) * 0.000000001).toInt();
+      return "${m}m${s > 0 ? " ${s}s" : ""}";
+    }
+  }
+}
+
 class _SettingArg {
   final String name;
   final List<String>? options;
@@ -52,8 +81,9 @@ class TstmInterpreter {
   late final List<_SettingArg> arguments;
 
   bool _running = false;
-  String _prompt = "\x1B[32m\\\x1B[31m>\x1B[0m ";
+  String _prompt = "\x1B[32m\\\x1B[34m>\x1B[0m ";
   List<OutputMode> _outModes = [OutputMode.value];
+  List<TimeMode> _timeModes = [TimeMode.none];
 
   TstmInterpreter({
     required this.reporter,
@@ -83,7 +113,7 @@ class TstmInterpreter {
     if (title) {
       _write("\x1B[34mTST\x1B[31mm \x1B[32mv\x1B[0m1.0.0\n");
       _write("exit using ctrl+c, or '.exit'\n");
-      _write("\x1B[33m'.help' for more information.\x1B[0m\n");
+      _write("\x1B[33m'.help' for more information.\x1B[0m\n\n");
     }
 
     _running = true;
@@ -91,6 +121,7 @@ class TstmInterpreter {
     RuntimeState.setup(source, reporter);
     while (_running) {
       String? input = _input(_prompt);
+      late final int lexerTime, parserTime, evalTime, endTime;
 
       if (input == null) break;
 
@@ -101,6 +132,8 @@ class TstmInterpreter {
 
       input = input.trim();
       source.src = input;
+      
+      lexerTime = _now();
       final tokens = _lexer.tokenize(input);
 
       if (reporter.hasErrors) {
@@ -108,6 +141,7 @@ class TstmInterpreter {
         continue;
       }
 
+      parserTime = _now();
       final expr = _parser.interpret(tokens);
 
       if (reporter.hasErrors) {
@@ -115,14 +149,18 @@ class TstmInterpreter {
         continue;
       }
 
+      evalTime = _now();
       final value = Evaluator.evaluate(expr, ctx);
 
       if (reporter.hasErrors) {
         _end(value);
         continue;
       }
+      
+      endTime = _now();
 
       _printOutput(value);
+      _printTime(lexerTime, parserTime, evalTime, endTime);
       _end(value);
     }
   }
@@ -146,6 +184,50 @@ class TstmInterpreter {
         output.write(' '); // separator
 
       final s = OutputMode.format(value, mode);
+      if (s != null)
+        output.write(s);
+    }
+
+    if (output.isNotEmpty)
+      _write('${output.toString()}\n');
+  }
+  
+  void _printTime(int lexerTime, int parserTime, int evalTime, int endTime) {
+    final StringBuffer output = StringBuffer();
+    for (int i = 0; i < _timeModes.length; i++) {
+      final mode = _timeModes[i];
+
+      if (mode == TimeMode.none) continue;
+
+      if (i != 0)
+        output.write('\n'); // separator
+
+      late final String? s;
+      switch (mode) {
+        case TimeMode.lexer:
+          s = "${mode.name}: ${TimeMode.format(parserTime - lexerTime)}";
+          break;
+        
+        case TimeMode.parser:
+          s = "${mode.name}: ${TimeMode.format(evalTime - parserTime)}";
+          break;
+        
+        case TimeMode.eval:
+          s = "${mode.name}: ${TimeMode.format(endTime - evalTime)}";
+          break;
+        
+        case TimeMode.compile:
+          s = "${mode.name}: ${TimeMode.format(evalTime - lexerTime)}";
+          break;
+        
+        case TimeMode.all:
+          s = "${mode.name}: ${TimeMode.format(endTime - lexerTime)}";
+          break;
+        
+        default:
+          s = null;
+      }
+      
       if (s != null)
         output.write(s);
     }
@@ -201,6 +283,8 @@ class TstmInterpreter {
   String _getOptionsHelp(_SettingArg arg) {
     return '\x1B[34m>>\x1B[0m ${arg.name} options\n\x1B[33m>\x1B[0m (${arg.options?.join(', ') ?? ''})';
   }
+  
+  int _now() => DateTime.now().microsecondsSinceEpoch;
 
   void _end([RuntimeValue? val]) {
     iteration?.call(val);
@@ -230,6 +314,7 @@ class TstmInterpreter {
       _SettingArg('.exit', null, (_, _, _) => stop()),
       _SettingArg('.clear', null, (_, _, _) => _write('\x1B[2J\x1B[H')),
       _SettingArg('.path', null, (_, _, _) => _write('${File('').absolute.path}\n')),
+      _SettingArg('.time', TimeMode.values.map<String>((e) => e.name).toList(), _argTime),
       _SettingArg('.help', null, _argHelp),
       _SettingArg('.prompt', null, _argPrompt),
       _SettingArg('.runmode', _runModesString, _argRunMode),
@@ -239,22 +324,26 @@ class TstmInterpreter {
     ];
   }
 
+  void _argTime(_SettingArg self, String input, List<int> selected) {
+    _timeModes = selected.map((i) => TimeMode.values[i]).toList();
+  }
+  
   void _argHelp(_SettingArg self, String input, List<int> selected) {
-    print(
+    _write(
 """\
 type expression to shell to get evaluated,
 or start with '.' to use shell arguments:
 
-- .exit: Stop the interpreter.
-- .clear: Clear screen.
-- .path: Show current path.
-- .help: Print this message.
-- .prompt: Change interpreter prefix.
-- .runmode: Set run print mode.
-- .run: Run tstm files.
-- .mode: Set interpreter output mode.
-- .print: Print different lists and states.\
-""");
+\x1B[33m- \x1B[34m.exit:\x1B[0m Stop the interpreter.
+\x1B[33m- \x1B[34m.clear:\x1B[0m Clear screen.
+\x1B[33m- \x1B[34m.path:\x1B[0m Show current path.
+\x1B[33m- \x1B[34m.help:\x1B[0m Print this message.
+\x1B[33m- \x1B[34m.prompt:\x1B[0m Change interpreter prefix.
+\x1B[33m- \x1B[34m.runmode:\x1B[0m Set run print mode.
+\x1B[33m- \x1B[34m.run:\x1B[0m Run tstm files.
+\x1B[33m- \x1B[34m.mode:\x1B[0m Set interpreter output mode.
+\x1B[33m- \x1B[34m.print:\x1B[0m Print different lists and states.\n"""
+    );
   }
 
   void _argPrompt(_SettingArg self, String input, List<int> selected) {
@@ -292,11 +381,14 @@ or start with '.' to use shell arguments:
       return;
     }
     
-    runner.run(
+    final res = runner.run(
       printTokens: _runModes.contains(1),
       printProgram: _runModes.contains(2),
       printResult: _runModes.contains(3),
     );
+    
+    if (res != null)
+      _printTime(res.lexerTime, res.parserTime, res.evalTime, res.endTime);
   }
 
   void _argMode(_SettingArg self, String input, List<int> selected) {
