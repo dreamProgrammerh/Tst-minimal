954a23d1c1b523fe9a97ea0198b30095860a0929
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2026-02-04 03:01:17 +0330

    update File(`completions.dart`): sort completions from the shortest to longest for better suggestions, and add global unmodifiable completions list.

--- implementations/Dart/shell/completions.dart
+++ implementations/Dart/shell/completions.dart
@@ -7,32 +7,41 @@ import '../primitives/literals/math.dart';
 import '../primitives/literals/special.dart';
 import 'repl.dart' as repl;
 
+late List<String> replCompletions;
+
 void initCompletions() {
+  final completions = <String>[];
+  
   // Add Color Names (blue, red, etc.)
   for (final c in colorLiterals)
-    repl.addCompletion(c.$1);
+    completions.add(c.$1);
     
   // Add special literals names (true, false, etc.)
   for (final l in specialLiterals)
-    repl.addCompletion(l.$1);
+    completions.add(l.$1);
   
   // Add math constants (PI, E, etc.)
   for (final m in mathLiterals)
-    repl.addCompletion(m.$1);
+    completions.add(m.$1);
   
   // Add color functions (hue(), hsl(), etc.)
   for (final colorf in colorFuncs)
-    repl.addCompletion('${colorf.$1}()');
+    completions.add('${colorf.$1}()');
   
   // Add math functions (abs(), min(), etc.)
   for (final mathf in mathFuncs)
-    repl.addCompletion('${mathf.$1}()');
+    completions.add('${mathf.$1}()');
   
   // Add solid functions (int(), float(), bool())
   for (final solidf in solidFuncs)
-    repl.addCompletion('${solidf.$1}()');
+    completions.add('${solidf.$1}()');
   
   // Add print functions (print(), printc(), etc.)
   for (final printf in printFuncs)
-    repl.addCompletion('${printf.$1}()');
+    completions.add('${printf.$1}()');
+    
+  completions.sort((a, b) => a.length - b.length);
+  
+  repl.addAllCompletion(completions);
+  replCompletions = List.unmodifiable(completions);
 }
