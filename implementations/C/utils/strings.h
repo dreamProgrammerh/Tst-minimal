862960e676b9b60eb677f905fe585d8978c09913
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2026-02-21 12:19:13 +0330

    update File(`string.h`): add string init shorthand functions.

--- implementations/C/utils/strings.h
+++ implementations/C/utils/strings.h
@@ -11,3 +11,16 @@ typedef struct string_t {
     const char* data;
     u32 length;
 } string_t;
+
+static inline
+str_t str_new(char* data, const u32 length) {
+    return (str_t){ .data=data, .length=length };
+}
+
+static inline
+string_t string_new(const char* data, const u32 length) {
+    return (string_t){ .data=data, .length=length };
+}
+
+#define str_lit(str) str_new((str), sizeof(str) - 1)
+#define string_lit(str) string_new((str), sizeof(str) - 1)
