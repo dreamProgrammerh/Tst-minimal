8b7af1cae9fd9bac6e740a1d6f9b907956c4ba5b
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2026-02-24 21:53:28 +0330

    update File(`errors.c`): make serr_format function work without using malloc and just memClone to allocate memory.

--- implementations/C/error/errors.c
+++ implementations/C/error/errors.c
@@ -1,13 +1,14 @@
 #include "errors.h"
 #include "../constants/const-errors.h"
-#include "../utils/strings.h"
 #include "../utils/memory.h"
 #include "../utils/position.h"
+#include "../utils/strings.h"
 
 #include <stdio.h>
-#include <stdlib.h>
 
 string_t serr_format(const SourceError* se, const Source src, const bool colored) {
+    char buffer[2048];
+
     const str_t fname = src.name == NULL
         ? str_lit("<anonymous>") : str_new(src.name, src.nameLength);
 
@@ -23,95 +24,70 @@ string_t serr_format(const SourceError* se, const Source src, const bool colored
     const u32 caretCount = se->length;
     const u32 underlineLen = spaceCount + caretCount;
 
-    // Mini arena for strings
-    const usize memLen = se->message.length +
-        fname.length + details.length + underlineLen + lineLen + 5; // +4 null terminator
-    void* mem = malloc(memLen);
-    usize memi = 0;
+    char underline[underlineLen];
 
-    // Build filename c string
-    char* fileNameStr = mem + memi;
-    memi += fname.length + 1;
-    for (u32 i = 0; i < fname.length; i++) fileNameStr[i] = fname.data[i];
-    fileNameStr[fname.length] = '\0';
-
-    // Build details c string
-    char* detailsStr = mem + memi;
-    memi += details.length + 1;
-    for (u32 i = 0; i < details.length; i++) detailsStr[i] = details.data[i];
-    detailsStr[details.length] = '\0';
-
-    // Build message c string
-    char* messageStr = mem + memi;
-    memi += se->message.length + 1;
-    for (u32 i = 0; i < se->message.length; i++) messageStr[i] = se->message.data[i];
-    messageStr[se->message.length] = '\0';
+    for (u32 i = 0; i < spaceCount; i++) underline[i] = ' ';
+    for (u32 i = spaceCount; i < underlineLen; i++) underline[i] = '^';
 
     // Get target line from src
-    char* lineStr = mem + memi;
-    memi += lineLen + 1;
-    const char* p = src.data + lineStart;
-    for (u32 i = 0; i < lineLen; i++) lineStr[i] = p[i];
-    lineStr[lineLen] = '\0';
-
-    // Build the inductor underline c string
-    char* underlineStr = mem + memi;
-    memi += underlineLen + 1;
-    for (u32 i = 0; i < spaceCount; i++) underlineStr[i] = ' ';
-    for (u32 i = spaceCount; i < underlineLen; i++) underlineStr[i] = '^';
-    underlineStr[underlineLen] = '\0';
+    const char* srcLine = src.data + lineStart;
 
     // Build result format string
-    char* result = malloc((memLen + 1024) * sizeof(char));
-
     u32 len = 0;
     if (colored) {
-        len = sprintf(result,
+        len = snprintf(buffer, sizeof(buffer),
             ErrClr_errorType"%s"
             ErrClr_punctuation"("
-            ErrClr_message"%s"
+            ErrClr_message"%.*s"
             ErrClr_punctuation")\n"
             "    "
             ErrClr_context"File "
-            ErrClr_name"%s "
+            ErrClr_name"%.*s "
             ErrClr_context"at "
             ErrClr_symbols"@"
             ErrClr_location"%u"
             ErrClr_symbols":"
             ErrClr_location"%u\n"
             "\n"
-            ErrClr_reset"%s\n"
-            ErrClr_caret"%s\n"
-            "%s"ErrClr_reset,
+            ErrClr_reset"%.*s\n"
+            ErrClr_caret"%.*s\n"
+            "%.*s"ErrClr_reset,
             SourceErrorKind_names[se->kind],
-            messageStr,
-            fileNameStr,
+            se->message.length, se->message.data,
+            fname.length, fname.data,
             line, col,
-            lineStr,
-            underlineStr,
-            detailsStr
+            lineLen, srcLine,
+            underlineLen, underline,
+            details.length, details.data
         );
     } else {
-        len = sprintf(result,
-            "%s(%s)\n"
-            "    File %s at @%u:%u\n"
+        len = snprintf(buffer, sizeof(buffer),
+            "%s(%.*s)\n"
+            "    File %.*s at @%u:%u\n"
             "\n"
-            "%s\n"
-            "%s\n"
-            "%s",
+            "%.*s\n"
+            "%.*s\n"
+            "%.*s",
             SourceErrorKind_names[se->kind],
-            messageStr,
-            fileNameStr,
+            se->message.length, se->message.data,
+            fname.length, fname.data,
             line, col,
-            lineStr,
-            underlineStr,
-            detailsStr
+            lineLen, srcLine,
+            underlineLen, underline,
+            details.length, details.data
         );
     }
 
-    char* clone = memClone(result, len);
-    free(result);
-    free(mem);
+    if (len >= sizeof(buffer)) {
+        // Truncation occurred - put three dot at end
+        buffer[sizeof(buffer)-1] = '.';
+        buffer[sizeof(buffer)-2] = '.';
+        buffer[sizeof(buffer)-3] = '.';
 
+        // TODO: add log file property at program if it not null,
+        //  log into that file.
+    }
+
+    char* clone = memClone(buffer, len);
     return (string_t) { .data = clone, .length = len };
 }
